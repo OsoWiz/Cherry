8624200266e17cf9238712df898a61b9682c1c0f
oso <otso.laasonen@aalto.fi>
2022-05-22 18:48:58 +0300

    uus testicommit

--- src/graphics/lowLevelGXSystem.h
+++ src/graphics/lowLevelGXSystem.h
@@ -5,3 +5,21 @@
 //Includes
 #include <glfw-3.3.7/include/GLFW/glfw3.h>
 #include <optional>
+
+//Useful structs
+struct QueueFamilyIndices
+{
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+
+    bool isComplete() {
+        return graphicsFamily.has_value() && presentFamily.has_value();
+    }
+
+};
+
+struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
